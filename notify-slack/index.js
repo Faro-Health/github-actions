7b8be7804ef42e5f33de6a20ce8a403ceb42996f
Maksym Kurhanetskyi <mkurhanetskyi@gmail.com>
2022-10-06 17:17:23 +0200

    Add notify-slack action

--- notify-slack/index.js
+++ notify-slack/index.js
@@ -4,11 +4,77 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
 
+const CONCLUSION_SUCCESS = 'success';
+const CONCLUSION_FAILURE = 'failure';
+
+function getFailedPayload(run) {
+    return {
+        text: `Build <${run.html_url}|'${run.display_title}'> failed`,
+        icon_emoji: ':rocket:',
+        attachments: [{
+            color: "#FF0000",
+            fields: [
+                {
+                    title: "Repository",
+                    value: `<${repository.html_url}|${repository.name}>`,
+                    short: true,
+                },
+                {
+                    title: "Branch",
+                    value: run.head_branch,
+                    short: true,
+                },
+                {
+                    title: "Requested for",
+                    value: `<${triggering_actor.html_url}|${triggering_actor.login}>`,
+                    short: true,
+                },
+                {
+                    title: "Duration",
+                    value: "20 seconds", // calc updated_at - created_at
+                    short: true,
+                },
+            ],
+        }],
+    };
+}
+
+function getSuccessPayload(run) {
+    return {
+        text: `Build <${run.html_url}|'${run.display_title}'> succeeded`,
+        icon_emoji: ':rocket:',
+        attachments: [{
+            color: "#00FF00",
+            fields: [
+                {
+                    title: "Repository",
+                    value: `<${repository.html_url}|${repository.name}>`,
+                    short: true,
+                },
+                {
+                    title: "Branch",
+                    value: run.head_branch,
+                    short: true,
+                },
+                {
+                    title: "Requested for",
+                    value: `<${triggering_actor.html_url}|${triggering_actor.login}>`,
+                    short: true,
+                },
+                {
+                    title: "Duration",
+                    value: "20 seconds", // calc updated_at - created_at
+                    short: true,
+                },
+            ],
+        }],
+    };
+}
+
 const main = async () => {
     try {
         const repository = core.getInput('repository', { required: true });
         const token = core.getInput('token', { required: true });
-        const message = core.getInput('message', { required: true });
         const conclusion = core.getInput('conclusion', { required: true });
         const slackWebHookUrl = core.getInput('slackWebHookUrl', { required: true });
 
@@ -22,25 +88,15 @@ const main = async () => {
             page: 1,
         });
         const [curRun, prevRun] = (data || {}).workflow_runs || [];
-        console.log(data.workflow_runs);
-        console.log("conclusion", conclusion);
-
-        const payload = {
-          attachments: [{text: message, color: 'green'}]
-        };
-        // const options = {
-        //   method: 'post',
-        //   baseURL: slackWebHookUrl,
-        //   headers: {
-        //     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-        //   },
-        //   data: payload,
-        // };
+        if (conclusion === CONCLUSION_SUCCESS) {
+            if ((prevRun || {}).conclusion === CONCLUSION_SUCCESS) {
+                return;
+            }
+        }
+        const payload = conclusion === CONCLUSION_FAILURE ? getFailedPayload(curRun) : getSuccessPayload(curRun);
         await axios.post(slackWebHookUrl, JSON.stringify(payload));
     } catch (error) {
         core.setFailed(error.stack);
     }
 }
-
-// Call the main function to run the action
 main();
